d5d96d1ebd672187d16307bac852030fc8f58879
ここあ <147463615+shiorason@users.noreply.github.com>
2024-07-19 14:40:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import json
 url=input('url : ')
 tex=requests.get(url)
 yuke=tex.text
-dat=yuke.find('<a href="')
+dat=yuke.find('<')
 new=yuke[dat:]
-dat2=new.find('>')
-dat3=new.find('</div>')
-honbun=new[dat2:dat3]
-newtext=honbun.replace('<br>','\n')
+dat2=new.find('曲名')
+dat3=new.find('')
+honbun=new[dat2:]
+newtext=honbun.replace('<br>','\n').replace('<div>','').replace('<br />','')\
+.replace('</div>','').replace('<li>','-')
 print(newtext)
